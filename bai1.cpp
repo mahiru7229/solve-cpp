5e040afbf2bea8c691cf5e8fb172ef2de3f31ad5
mahiru7229 <110292918+mahiru7229@users.noreply.github.com>
2023-09-22 16:02:50 +0700

    Create bai1.cpp

---
+++ bai1.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+bool so_doi_xung(long long x) {
+    string s = to_string(x);
+    int left = 0;
+    int right = s.size() - 1;
+    while (left < right) {
+        if (s[left] != s[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+
+bool tat_ca_chu_so_giong_nhau(long long x) {
+    string s = to_string(x);
+    char first_digit = s[0];
+    for (char c : s) {
+        if (c != first_digit) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    long long n, m = 0;
+    cin >> n;
+    if (n < 10) {
+        m = 11 - n;
+    } else {
+        if (so_doi_xung(n)) {
+            m = 0;
+        } else {
+            long long so_doi_xung_ = n + 1;
+            while (true) {
+                if (tat_ca_chu_so_giong_nhau(so_doi_xung_)) {
+                    m = so_doi_xung_ - n;
+                    break;
+                }
+                so_doi_xung_++;
+            }
+        }
+    }
+    cout << m;
+}
